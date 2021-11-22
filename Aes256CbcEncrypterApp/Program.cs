509c66c976d49e7c86287d9322e01f673e4ce08d
Taku <Taku@TAKU-LENOVO>
2021-11-22 10:34:15 +0300

    Add project files.

---
+++ Aes256CbcEncrypterApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Aes256CbcEncrypterApp
+{
+    internal class Program
+    {
+        private static readonly string key = "728378c43ebb41df9495bc090ecf54af";
+        private static readonly string initVector = key[..16];
+
+        private static AesManaged CreateAes()
+        {
+            using AesManaged aes = new()
+            {
+                Key = Encoding.UTF8.GetBytes(key),
+                IV = Encoding.UTF8.GetBytes(initVector)
+            };
+            return aes;
+        }
+
+        public static string Encrypt(string text)
+        {
+            using var aes = CreateAes();
+            ICryptoTransform encryptor = aes.CreateEncryptor();
+            using MemoryStream ms = new();
+            using CryptoStream cs = new(ms, encryptor, CryptoStreamMode.Write);
+            using (StreamWriter sw = new(cs))
+            {
+                sw.Write(text);
+            }
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public static string Decrypt(string text)
+        {
+            using var aes = CreateAes();
+            ICryptoTransform decryptor = aes.CreateDecryptor();
+            using MemoryStream ms = new(Convert.FromBase64String(text));
+            using CryptoStream cs = new(ms, decryptor, CryptoStreamMode.Read);
+            using StreamReader reader = new(cs);
+            return reader.ReadToEnd();
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String");
+            Console.WriteLine();
+            string originalString = Console.ReadLine();
+            Console.WriteLine("---------------------------------------------");
+            var encryptedString = Encrypt(originalString);
+            Console.WriteLine();
+            Console.WriteLine("Encrypeted Text");
+            Console.WriteLine(encryptedString);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Enter Encrypeted String");
+            Console.WriteLine();
+            string newecryptedString = Console.ReadLine();
+            Console.WriteLine();
+            var decryptedString = Decrypt(newecryptedString);
+            Console.WriteLine();
+            Console.WriteLine("Decrypted");
+            Console.WriteLine();
+            Console.WriteLine(decryptedString);
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
+        }
+    }
+}
